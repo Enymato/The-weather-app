ac6d8143527c2c8fb82ac42756746b87d83ae63b
Enymato <79268301+Enymato@users.noreply.github.com>
2021-04-09 16:57:22 -0500

    implemented unit convertion

--- style/java.js
+++ style/java.js
@@ -29,6 +29,7 @@ let descriptionElement= document.querySelector("#description");
 let humidityElement= document.querySelector("#humidity");
 let windElement= document.querySelector("#wind");
 let timeElement= document.querySelector("#dayTime")
+celsiusTemperature= response.data.main.temp;
 temperatureElement.innerHTML= Math.round (response.data.media.temp);
 cityElement.innerHTML= response.data.name;
 descriptionElement.innerHTML= response.data.weather[0].description;
@@ -40,11 +41,9 @@ timeElement.innerHTML= formatDate(response.data.dt * 1000);
 function search(city){
 
     let apikey="9a3263bb7dd95aedc9d9609db37bad89";
-    let city= "Mexico";
     let apiUrl= `https://api.openweathermap.org/data/2.5/weather?
-        q={city}&appid={apikey}&units=metric`;
-        
-axios.get(apiUrl). then(displayTemperature);
+        q={city}&appid={apiKey}&units=metric`;
+      axios.get(apiUrl).then(displayTemperature);
 }
 function countryData(){
     event.preventDefault();
@@ -53,7 +52,36 @@ function countryData(){
 
 }
 
+function showFahrenheitTemperature(event){
+   event.preventDefault();
+    let temperatureNumber= document.querySelector("#temperature");
+        celsiusLink.classList.remove("active");
+        fahrenheitLink.classList.add("active");
+    let fahrenheitTemperature= (celsiusTemperature * 9) / 5 + 32 ;
+    temperatureNumber.innerHTML= Math.round (fahrenheitTemperature);
+
+}
+
+function showCelsiusTemperature(event){
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+        fahrenheitLink.classList.remove("active");
+    let temperatureNumber= document.querySelector("#temperature");
+    temperatureNumber.innerHTML= Math.round (celsiusTemperature);
 
 
-let form= documen.querySelector("#search");
+}
+
+let celsiusTemperature= null;
+
+
+
+let form= document.querySelector("#search");
 form.addEventListener("submit", countryData);
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+search("Mexico");
