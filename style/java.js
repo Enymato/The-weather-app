fbffd0830cabde64b20b0e422695b3fd16d907db
Enymato <79268301+Enymato@users.noreply.github.com>
2021-04-27 23:55:01 -0500

    changing things

--- style/java.js
+++ style/java.js
@@ -75,8 +75,7 @@ let descriptionElement= document.querySelector("#description");
 let humidityElement= document.querySelector("#humidity");
 let windElement= document.querySelector("#wind");
 let timeElement= document.querySelector("#dayTime");
-let imageElement=document.querySelector("#pibuImage");
-
+let iconElement= document.querySelector("#icon");
 
 
 celsiusTemperature= response.data.main.temp;
@@ -86,7 +85,11 @@ descriptionElement.innerHTML= response.data.weather[0].description;
 humidityElement.innerHTML= response.data.main.humidity;
 windElement.innerHTML= Math.round (response.data.wind.speed);
 timeElement.innerHTML= formatDate(response.data.dt * 1000);
-
+iconElement.setAttribute(
+    "src",
+     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}s@2x.png`
+     );
+      iconElement.setAttribute("alt", response.data.weather[0].description);
 
 
 
@@ -94,6 +97,8 @@ timeElement.innerHTML= formatDate(response.data.dt * 1000);
 getForecast(response.data.coord);
 
 }
+
+
 
 function search(city){
 
